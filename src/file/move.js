8d8064d66cb601473ff781ba9977b505f044a330
taleatg <t_korbyt@mail.ru>
2022-06-10 23:55:17 +0300

    feat: add move and copy files

--- src/file/move.js
+++ src/file/move.js
@@ -1,10 +1,48 @@
-import fs from 'fs';
+import fs, { access, constants } from 'fs';
+import { join, basename } from 'path';
 
-export const move = (path) => {
-  // try {
-  //   const stream = fs.createReadStream(path, {encoding: 'utf8'});
-  //   stream.pipe(process.stdout);
-  // } catch {
-  //   process.stdout.write('\nOperation failed\n');
-  // }
+//TODO: fix path to directory
+
+export const copy = async (args) => {
+  const [pathRead, pathWrite] = [...args];
+
+  if (!pathRead || !pathWrite) {
+    process.stdout.write('\nOperation failed\n');
+    return;
+  }
+
+  const fileName = basename(pathRead);
+
+  try {
+    await fs.promises.access(pathWrite);
+
+    access(pathRead, constants.F_OK, (err) => {
+      err
+        ? process.stdout.write('\nOperation failed: file not found\n')
+        : access(join(pathWrite, fileName), constants.F_OK, async (err) => {
+
+          if (err) {
+            const readStream = fs.createReadStream(pathRead);
+            const writeStream = fs.createWriteStream(join(pathWrite, fileName));
+
+            await readStream.pipe(writeStream);
+
+            readStream.on('end', () => {
+              fs.unlink(pathRead, (err) => {
+                if (err) {
+                  process.stdout.write('\nOperation failed');
+                }
+              });
+            });
+
+          } else {
+            process.stdout.write('\nOperation failed: file already exists\n');
+          }
+
+        });
+    });
+
+  } catch {
+    process.stdout.write('\nOperation failed\n');
+  }
 }
